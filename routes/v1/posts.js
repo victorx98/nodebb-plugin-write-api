8375419e99514cd0961f36ae272a0be5996de551
river <revir.qing@gmail.com>
2017-08-25 23:22:58 +0800

    add bookmark and vote api

--- routes/v1/posts.js
+++ routes/v1/posts.js
@@ -2,43 +2,84 @@
 /* globals module, require */
 
 var posts = require.main.require('./src/posts'),
-	apiMiddleware = require('./middleware'),
-	errorHandler = require('../../lib/errorHandler'),
-	utils = require('./utils');
+  User = require.main.require('./src/user'),
+  db = require.main.require('./src/database'),
+  apiMiddleware = require('./middleware'),
+  errorHandler = require('../../lib/errorHandler'),
+  async = require('async'),
+  utils = require('./utils');
 
 
 module.exports = function(middleware) {
-	var app = require('express').Router();
+  var app = require('express').Router();
 
-	app.route('/:pid')
-		.put(apiMiddleware.requireUser, function(req, res) {
-			if (!utils.checkRequired(['content'], req, res)) {
-				return false;
-			}
+  app.route('/:pid')
+    .put(apiMiddleware.requireUser, function(req, res) {
+      if (!utils.checkRequired(['content'], req, res)) {
+        return false;
+      }
 
-			var payload = {
-				uid: req.user.uid,
-				pid: req.params.pid,
-				content: req.body.content,
-				options: {}
-			};
+      var payload = {
+        uid: req.user.uid,
+        pid: req.params.pid,
+        content: req.body.content,
+        options: {}
+      };
 
-			if (req.body.handle) { payload.handle = req.body.handle; }
-			if (req.body.title) { payload.title = req.body.title; }
-			if (req.body.topic_thumb) { payload.options.topic_thumb = req.body.topic_thumb; }
-			if (req.body.tags) { payload.options.tags = req.body.tags; }
+      if (req.body.handle) { payload.handle = req.body.handle; }
+      if (req.body.title) { payload.title = req.body.title; }
+      if (req.body.topic_thumb) { payload.options.topic_thumb = req.body.topic_thumb; }
+      if (req.body.tags) { payload.options.tags = req.body.tags; }
 
-			posts.edit(payload, function(err) {
-				errorHandler.handle(err, res);
-			})
-		})
-		.delete(apiMiddleware.requireUser, function(req, res) {
-			posts.delete(req.params.pid, req.user.uid, function(err) {
-				errorHandler.handle(err, res);
-			});
-		});
+      posts.edit(payload, function(err) {
+        errorHandler.handle(err, res);
+      })
+    })
+    .delete(apiMiddleware.requireUser, function(req, res) {
+      posts.delete(req.params.pid, req.user.uid, function(err) {
+        errorHandler.handle(err, res);
+      });
+    });
 
-		
+  app.route('/:pid/bookmark').post(apiMiddleware.requireUser, function(req, res) {
+    posts.bookmark(req.params.pid, req.user.uid, function(err) {
+      errorHandler.handle(err, res);
+    })
+  });
+  app.route('/:pid/unbookmark').post(apiMiddleware.requireUser, function(req, res) {
+    posts.unbookmark(req.params.pid, req.user.uid, function(err) {
+      errorHandler.handle(err, res);
+    })
+  });
+  app.route('/:pid/bookmarked-users').get(apiMiddleware.requireUser, function(req, res) {
+    var pid = req.params.pid;
+    async.waterfall([
+      function(next) {
+        db.getSetMembers('pid:'+pid+':users_bookmarked', next)
+      },
+      function(members, next) {
+        User.getUsersData(members, next);
+      }
+    ], function(err, data) {
+      errorHandler.handle(err, res, data);
+    })
+  });
 
-	return app;
+  app.route('/:pid/upvote').post(apiMiddleware.requireUser, function(req, res) {
+    posts.upvote(req.params.pid, req.user.uid, function(err, data) {
+			errorHandler.handle(err, res, data);
+    });
+  });
+	app.route('/:pid/downvote').post(apiMiddleware.requireUser, function(req, res) {
+    posts.downvote(req.params.pid, req.user.uid, function(err, data) {
+      errorHandler.handle(err, res, data);
+    });
+  });
+	app.route('/:pid/unvote').post(apiMiddleware.requireUser, function(req, res) {
+    posts.unvote(req.params.pid, req.user.uid, function(err, data) {
+      errorHandler.handle(err, res, data);
+    });
+  });
+
+  return app;
 };
